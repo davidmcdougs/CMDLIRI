ec6a41a613301db54bc8d083a3f73e784d316213
David McDougald <dmcdougald@metrohealth.org>
2018-01-04 18:09:03 -0500

    some steps done

---
+++ keys.js
@@ -0,0 +1,11 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+	//if fails, remove <>
+  consumer_key: '<cmqVanvfgbogYh4L3zN2j63SK>',
+  consumer_secret: '<xGta7F53VQHdKMFaaNlnOCwUtAs3HHevpUmCOoLtdnUf4bnly3>',
+  access_token_key: '<949045312664014848-kusDQNYCPw6XI886qldAYXPlHWb5DBy>',
+  access_token_secret: '<GiJXacAfPbFvn4KQD92Fo17g46ZiiqAylJABX0kPZSz2b>',
+}
+
+module.exports = twitterKeys;
